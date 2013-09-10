5d691a8842fefcd3c5225bcea844c3a5c6792c13
transistorgrab <github@transistorgrab.de>
2013-09-10 21:07:12 +0200

    new header file, new defines

--- digiamp.c
+++ digiamp.c
@@ -8,14 +8,10 @@
 
 #include <avr/io.h>
 #include <avr/iomx8.h>
+#include "digiamp.h"	/** defines for the project	*/
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
-}
+
+
 
 /** \brief: initiates all controller pins for input or output
 
@@ -23,35 +19,36 @@ int main(void)
 */
 void init_ports(void)
 {
-							/** Port B0 unused						*/
-							/** Port B1 unused						*/
-							/** Port B2 unused						*/
-	DDRB = (1 << DDB3);		/** Port B3 input, AVR_MOSI				*/
-	DDRB = (1 << DDB4);		/** Port B4 output, AVR_MISO			*/
-	DDRB = (1 << DDB5);		/** Port B5 input, AVR_SCK				*/
-	DDRB = (1 << DDB6);		/** Port B6 output, CS2 for LED			*/
-	DDRB = (1 << DDB7);		/** Port B7 output, MUX_AUDIO_OFF		*/
-	DDRC = (1 << DDC0);		/** Port C0 output, MUX_SOURCE_SEL_0	*/
-	DDRC = (1 << DDC1);		/** Port C1 output, MUX_SOURCE_SEL_1	*/
-	DDRC = (1 << DDC2);		/** Port C2 output, PA_MUTE				*/
-	DDRC = (1 << DDC3);		/** Port C3 output, SCLK				*/
-	DDRC = (1 << DDC4);		/** Port C4 output, CS1					*/
-	DDRC = (1 << DDC5);		/** Port C5 output, SDAT				*/
-	DDRC = (1 << DDC6);		/** Port C6 input, RESET				*/
-							/** Port C7 not available				*/
-							/** Port D0 unused						*/
-	DDRD = (0 << DDD1);		/** Port D1 input, incremental input A	*/
-	DDRD = (0 << DDD2);		/** Port D2 input, incremental input B	*/
-	DDRD = (0 << DDD3);		/** Port D3 output, SDAT				*/
-	DDRD = (0 << DDD4);		/** Port D4 output, SDAT				*/
-							/** Port D5 not available				*/
-							/** Port D6 not available				*/
-							/** Port D7 not available				*/
+	DDRB = DDRB_SETTING;
+	DDRC = DDRC_SETTING;
+	DDRD = DDRD_SETTING;
 
-	PORTC = (0 << PC0) | (0 << PC1);				/** select MUX source 0		*/
-	PORTC = (1 << PC2);								/** unmute power amplifier	*/
-	PORTB = (0 << PB7);								/** unmute MUX				*/
+	MUX_SOURCE_SEL_0 = 0;		/** select MUX source 0		*/
+	MUX_SOURCE_SEL_1 = 0;
 	
-	PORTC = (0 << PC3) | (1 << PC4) | (0 << PC5);	/** initiate Soft-SPI pins	*/
-	PORTB = (0 << PB6);								/** initiate Soft-SPI pins	*/
+	AMP_ENABLE = 1;				/** unmute power amplifier	*/
+	
+	MUX_AUDIO_OFF = 0;			/** unmute MUX				*/
+	
+	SPI_CLK = 0;				/** initiate Soft-SPI pins	*/
+	SPI_DAT = 0;
+	SPI_CS1_ATT = 1;
+	SPI_CS2_LED = 1;
+}
+
+/** init the timer for the scheduler cycles	*/
+void init_timer(void)
+{
+	
+}
+
+int main(void)
+{
+	init_ports();
+	init_timer();
+	
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
 }
---
+++ digiamp.h
@@ -0,0 +1,72 @@
+/** project file for all project specific defines and functions	*/
+/** project uses ATMEGA88AU (http://www.atmel.com/images/doc2545.pdf)	*/
+
+/** LEDs	*/
+#define LED_SOURCE_1
+#define LED_SOURCE_2
+#define LED_SOURCE_3
+#define LED_SOURCE_4
+
+#define LED_VOLUME_1
+#define LED_VOLUME_2
+#define LED_VOLUME_3
+#define LED_VOLUME_4
+
+/** defines for ports **
+**
+** hardware pin configuration:
+** all not defined ports are not used or not available	*/
+
+/** function and struct for setting single port bits	
+    source: [http://www.mikrocontroller.net/attachment/27445/SBIT.C] */
+struct bits {
+	uint8_t b0:1;
+	uint8_t b1:1;
+	uint8_t b2:1;
+	uint8_t b3:1;
+	uint8_t b4:1;
+	uint8_t b5:1;
+	uint8_t b6:1;
+	uint8_t b7:1;
+} __attribute__((__packed__));
+
+#define SBIT(port,pin) ((*(volatile struct bits*)&port).b##pin)
+
+/** Hardware SPI (programmer) Interface	*/
+#define AVR_MOSI	( PINB, 3 )		/** hardware SPI MOSI pin	*/
+#define AVR_MISO	( PORTB, 4 )	/** hardware SPI MISO pin	*/
+#define AVR_SCK		( PINB, 5 )		/** hardware SPI clock pin	*/
+
+
+/** SPI Port for Soft SPI, outputs	*/
+#define SPI_CLK			SBIT( PORTC, 3 )	/** SPI clock line		*/
+#define SPI_DAT			SBIT( PORTC, 5 )	/** SPI data line		*/
+#define SPI_CS1_ATT		SBIT( PORTC, 4 )	/** SPI for attenuator	*/
+#define SPI_CS2_LED		SBIT( PORTB, 6 )	/** SPI for LEDs		*/
+
+/** ports for Multiplexer control, outputs	*/
+#define MUX_AUDIO_OFF		SBIT( PORTC, 7 )	/** disables output signal from multiplexer	*/
+#define MUX_SOURCE_SEL_0	SBIT( PORTC, 0 )	/** multiplexer select signal bit 0			*/
+#define MUX_SOURCE_SEL_1	SBIT( PORTC, 1 )	/** multiplexer select signal bit 1			*/
+
+/** ports for power amplifier control and status	*/
+#define AMP_ENABLE			SBIT( PORTC, 2 )	/** enables output stage of power amplifier	*/
+#define AMP_WARNING			SBIT( PIND, 4 )		/** input for amplifier warning				*/
+#define AMP_ERROR			SBIT( PIND, 5 )		/** input for amplifier error				*/
+
+/** ports for incremental encoder, inputs	*/
+#define VOL_A	SBIT( PIND, 1 )	/** incremental encoder output A	*/
+#define VOL_B	SBIT( PIND, 2 )	/** incremental encoder output A	*/
+
+/** port for source select switch	*/
+#define SOURCE_SW	SBIT( PIND, 3 ) /** input for source select button	*/
+
+/** port for IR data input	*/
+#define IR_DATA		SBIT( PIND, 6 ) /** input for data from IR receiver	*/
+
+/** direction defines according to above defines: 0=input, 1=output	*/
+#define DDRB_SETTING	0x68	/** Port B direction setting iooi_oiii	*/
+#define DDRC_SETTING	0x3F	/** Port C direction setting iioo_oooo	*/
+#define DDRD_SETTING	0x00	/** Port D direction setting	iiii_iiii */
+
+/** project functions	*/
