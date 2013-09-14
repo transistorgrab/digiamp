c1af3d6f10abb8622f18558ba5140b9e710ed3ce
transistorgrab <github@transistorgrab.de>
2013-09-14 23:20:10 +0200

    added output.c, corrected typo in input.c

---
+++ output.c
@@ -0,0 +1,15 @@
+/** this file contains all functions for output operations	*/
+
+#include "digiamp.h"
+
+/** this function sets the LEDs via Soft SPI	*/
+void set_leds(int led_vector)
+{
+	
+}
+
+/** this function sets the volume via Soft SPI	*/
+void set_volume(int volume_right, int volume_left)
+{
+	
+}
